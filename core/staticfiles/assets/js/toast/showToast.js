7e3bf4dd88f4f18deb423718b731a2550ccece96
Mikiyas <mikiyas.m.degefu@gmail.com>
2025-08-07 12:25:13 +0300

    fix: staticfiles removed

--- core/staticfiles/assets/js/toast/showToast.js
+++
@@ -1,9 +0,0 @@
-showToast = (title, body, cssClass)=> {
-    $.toast({
-            heading: title,
-            text: body,
-            showHideTransition: 'slide',
-            icon: cssClass,
-            position: 'top-right',
-            })
-}
