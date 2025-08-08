a3d9e2f4ae7c315b5bf72689508ad21f82b7c395
Mikiyas <mikiyas.m.degefu@gmail.com>
2025-08-08 10:11:23 +0300

    fix: poetry

--- core/mobile/admin.py
+++ core/mobile/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-from .models import MobileDahboardOverview
+from import_export import resources
+from .models import *
+
 
 
 class MobileDahboardOverviewResource(resources.ModelResource):
     class Meta:
         model = MobileDahboardOverview
-        fields = '__all__'  # Export all fields
-        export_order = ('id',)  # Optional: set export order
-
+        
 
 class MobileDahboardOverviewAdmin(ImportExportModelAdmin):
     resource_class = MobileDahboardOverviewResource
