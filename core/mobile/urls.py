14d2d7a6f47a5a245e7706614596c63ccb821dd8
KaleabHegie <benjiyg400@gmail.com>
2025-08-08 11:52:08 +0300

    fix:url added of initiatives

--- core/mobile/urls.py
+++ core/mobile/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('indicator-detail/<str:id>/' , api.mobile_indicator_detail),
     path('indicator-performance-detail/<str:id>/' , api.indicator_performance_detail),
     path('project-list/' , api.mobile_projects),
+    path('initiative-list/' , api.mobile_initiatives),
     path('project-detail/<str:id>/' , api.mobile_project_detail),
     path('search-auto-complete/<str:id>/', api.mobile_topic_detail_search, name='mobile_topic_detail_search'),
     path('general_search/' , api.general_search),
