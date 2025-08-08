1812a968bb5cc4359ff4c23b81d93202feb30914
Mikiyas <mikiyas.m.degefu@gmail.com>
2025-08-08 09:53:19 +0300

    fix: admin dashabord import export added

--- core/mobile/admin.py
+++ core/mobile/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import MobileDahboardOverview
 
 
-class MobileDahboardOverviewAdmin(admin.ModelAdmin):
-    autocomplete_fields = ['indicator'] 
+class MobileDahboardOverviewResource(resources.ModelResource):
+    class Meta:
+        model = MobileDahboardOverview
+        fields = '__all__'  # Export all fields
+        export_order = ('id',)  # Optional: set export order
+
+
+class MobileDahboardOverviewAdmin(ImportExportModelAdmin):
+    resource_class = MobileDahboardOverviewResource
+    autocomplete_fields = ['indicator']
 
 
 admin.site.register(MobileDahboardOverview, MobileDahboardOverviewAdmin)
