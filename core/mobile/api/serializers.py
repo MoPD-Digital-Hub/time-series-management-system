6b56d08645c00d1aea1cddda5ccb8e3e598d3049
Mikiyas <mikiyas.m.degefu@gmail.com>
2026-01-15 11:34:24 +0300

    fix: quarter data ordering

--- core/mobile/api/serializers.py
+++ core/mobile/api/serializers.py
@@ -676,9 +676,9 @@ class IndicatorShortSerializer(serializers.ModelSerializer):
             qs = qs.filter(for_quarter__title_ENG=quarter)
 
         qs = qs.annotate(
-        year_ec_int=Cast('for_datapoint__year_EC', IntegerField()),
-        quarter_int=Cast('for_quarter__number', IntegerField()), 
-    ).order_by('-year_ec_int', '-quarter_int')[:12]
+            year_ec_int=Cast('for_datapoint__year_EC', IntegerField()),
+        ).order_by('-year_ec_int', '-for_quarter__number')
+
 
         return QuarterDataSerializer(list(qs)[::-1], many=True).data
 
