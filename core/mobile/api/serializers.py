ecf29544af7ee11c67165fe138919a25969bd436
Mikiyas <mikiyas.m.degefu@gmail.com>
2026-01-26 12:30:02 +0300

    fix: month data only keep last 20 items

--- core/mobile/api/serializers.py
+++ core/mobile/api/serializers.py
@@ -710,18 +710,18 @@ class IndicatorShortSerializer(serializers.ModelSerializer):
 
         qs = obj.month_data.all()
 
-        # if year and month:
-        #     qs = qs.filter(
-        #         for_datapoint__year_EC=year,
-        #         for_month__month_ENG=month
-        #     )
-        #     return MonthDataSerializer(qs[:1], many=True).data
+        if year and month:
+            qs = qs.filter(
+                for_datapoint__year_EC=year,
+                for_month__month_ENG=month
+            )
+            return MonthDataSerializer(qs[:1], many=True).data
 
-        # if year:
-        #     qs = qs.filter(for_datapoint__year_EC=year)
+        if year:
+            qs = qs.filter(for_datapoint__year_EC=year)
 
-        # if month:
-        #     qs = qs.filter(for_month__month_ENG=month)
+        if month:
+            qs = qs.filter(for_month__month_ENG=month)
 
         qs = qs.order_by('-for_datapoint__year_EC', '-for_month__number')[:20][::-1]
         month_list = list(qs)
